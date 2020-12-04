6eef2738fb8c33062de58ec34936b341aac076c9
PedroExpedito <expeditocaf00@gmail.com>
2020-12-04 10:51:09 -0300

    muito a aprender

--- tier-1/calculator/script.js
+++ tier-1/calculator/script.js
@@ -1,24 +1,28 @@
-const display = document.getElementById("display");
+var equal = false;
 
-const key_1 = document.getElementById("nu_1")
-const key_2 = document.getElementById("nu_2")
-const key_3 = document.getElementById("nu_3")
-const key_4 = document.getElementById("nu_4")
-const key_5 = document.getElementById("nu_5")
-const key_6 = document.getElementById("nu_6")
-const key_7 = document.getElementById("nu_7")
-const key_8 = document.getElementById("nu_8")
-const key_9 = document.getElementById("nu_9")
-
-let value = [];
-
-key_1.onclick = function() {
-  value.push(1);
-  render();
+function bc(fn){
+  // tratamento
+  fn.replace(/[^-()\d/*+.]/g, '');
+  return new Function('return ' + fn)();
 }
-
-function render() {
-  display.value = value.join("");
+function insert(num){
+  if(equal) {
+    clean();
+    equal = false;
+  }
+  document.form.textbox.value = document.form.textbox.value+num;
 }
-
-
+function clean(){
+  document.form.textbox.value = '';
+}
+function total(){
+  var x = document.form.textbox.value;
+  if(x){
+    equal = true;
+    document.form.textbox.value = bc(x);
+  }
+}
+function back(){
+  var exp = document.form.textbox.value;
+  document.form.textbox.value = exp.substring(0, exp.length-1);
+}
